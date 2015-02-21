2274caa4624601e3d39dae92b69c38eefdba3e2b
Jason Ryan <jason.ryan@audiologydesign.com>
2015-02-21 12:33:10 -0500

    Created dev branch for oneframework

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+	    dist: {
+	      options: {
+	        style: 'expanded',
+	        precision: 9,
+	        sourcemap: 'none'
+	      },
+	      files: {
+	        'css/oneframework/oneframework.css': 'css/oneframework/scss/oneframework.scss'
+	      }
+	    }
+	  },
+	  cssmin: {
+		  target: {
+		    files: [{
+		      expand: true,
+		      cwd: 'css/oneframework',
+		      src: ['*.css', '!*.min.css'],
+		      dest: 'css/oneframework',
+		      ext: '.min.css'
+		    }]
+		  }
+		},
+		watch: {
+			options: {
+				livereload: true
+			},
+			sass: {
+				files: ['css/oneframework/scss/**/*.scss'],
+				tasks: ['sass','cssmin']
+			}
+	  },
+	  express:{
+	  	all: {
+	  		options: {
+	  			port: 9000,
+	  			hostname: 'localhost',
+	  			bases: ['./'],
+	  			livereload: true
+	  		}
+	  	}
+	  }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express');
+
+  grunt.registerTask('default', ['sass','watch']); // without livereload
+  grunt.registerTask('server', ['express','watch']);  // with livereload
+};
--- README.md
+++ README.md
@@ -11,3 +11,12 @@ speed up development time while establishing universal standards across client s
 There are a number of _scss partials_ created that can be easily commented out if you shall choose not to inlcude them and
 include your own, making everything interchangable.
 
+###Getting Started
+
+Currently configured with Npm and & Grunt task manager to compile, minify, and livereload the page. This is just for development only 
+on the framework, to use the production version, use _master_.
+
+You can install Node, Npm, and Grunt by following these instructions [here](http://thechangelog.com/install-node-js-with-homebrew-on-os-x/) 
+
+To install dependencies, _cd_ to the _root_ directory and run _npm install_ and it will look through your _package.json_ file and install
+the necessary dependencies.
